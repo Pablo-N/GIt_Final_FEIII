112c00c9d11d265771a97077406c925e215a3074
Detobone <detobone22@gmail.com>
2022-12-03 17:18:53 -0500

    Styling the navbar

--- fe3-final-main/src/Components/Navbar.jsx
+++ fe3-final-main/src/Components/Navbar.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
-const Navbar = (props) => {
-
+const Navbar = () => {
   return (
-    <header>
-      
-    <nav>
-      <Link to="/home">Home</Link>      
-      <Link to="/contact">Contact</Link>
-      <Link to="/Favs">Favs</Link>
-      <button >Change theme</button>
-    </nav>
+    <header className="navigation">
+      <nav className="navigation__container">
+        <Link to="/home">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/Favs">Favs</Link>
+        <button>Change theme</button>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- fe3-final-main/src/index.jsx
+++ fe3-final-main/src/index.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './styles/main.css';
-import App from './App';
-import Home from './Routes/Home'
-import Contact from "./Routes/Contact"
-import Favs from "./Routes/Favs"
-import Detail from "./Routes/Detail"
-import {ContextProvider} from "./Components/utils/global.context"
+import "./styles/main.css";
+import App from "./App";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} >
-            <Route index path="/home" element={<Home />} />
-            <Route index path="/contact" element={<Contact />} />
-            <Route path="/dentist/:id" element={<Detail />} />
-            <Route path="/favs" element={<Favs />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>  
-
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index path="/home" element={<Home />} />
+          <Route index path="/contact" element={<Contact />} />
+          <Route path="/dentist/:id" element={<Detail />} />
+          <Route path="/favs" element={<Favs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
