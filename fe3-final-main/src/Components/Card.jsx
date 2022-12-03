80641c6f7abf07402e07bbdd47892412afaa830b
Detobone <detobone22@gmail.com>
2022-12-03 01:37:21 -0500

    Typographies and card animations had been added with scss

--- fe3-final-main/src/Components/Card.jsx
+++ fe3-final-main/src/Components/Card.jsx
@@ -1,20 +1,49 @@
 import React from "react";
 
-
-const Card = ({ name, username, id }) => {
-
-  const addFav = ()=>{
+const Card = ({ dentist }) => {
+  const { name, username, id } = dentist;
+  const addFav = () => {
     // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  };
 
   return (
-    <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+    <div key={id} className="card">
+      {/* En cada card deberan mostrar en name - username y el id */}
+      <div className="card__side card__side--front">
+        <div className="card__picture card__picture--3">&nbsp;</div>
+        <h4 className="card__heading">
+          <span className="card__heading-span card__heading-span--3">
+            {name}
+          </span>
+        </h4>
+        <div className="card__details">
+          <ul>
+            {/* <li><img src="../images/doctor.jpg"/></li> */}
+            <li>Name: {name}</li>
+            <li>Username:{username} </li>
+          </ul>
+        </div>
+      </div>
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+      <div className="card__side card__side--back card__side--back-3">
+        <div className="card__cta">
+          <div className="card__price-box">
+            <p className="card__price-only">Only</p>
+            <p className="card__price-only">{name}</p>
+          </div>
+          <a href="#popup" className="btn btn--white">
+            Book now!
+          </a>
+          <a href="#popup" className="btn btn--white">
+            Show more details
+          </a>
+        </div>
+      </div>
 
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+
+      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+      {/* <button onClick={addFav} className="favButton">Add fav</button> */}
     </div>
   );
 };
--- fe3-final-main/src/Routes/Home.jsx
+++ fe3-final-main/src/Routes/Home.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import Card from '../Components/Card'
+import { useEffect, useState } from "react";
+import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -9,44 +9,26 @@ const Home = () => {
   const [dentists, setDentists] = useState([]);
 
   const handleFetchRequest = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const jsonResponse = await response.json();
-    setDentists([...jsonResponse])
-  }
+    setDentists([...jsonResponse]);
+  };
 
   useEffect(() => {
     handleFetchRequest();
-  }, [])
-
-
-
+  }, []);
 
   return (
+    //card-grid
     <main>
       <h1>Home</h1>
-      <div className='card-grid'>
+      <div className="cardContainer">
         {dentists.map((dentist) => (
-          <div className="card__details">
-            <ul>
-              <li><img src="../images/doctor.jpg"/></li>
-              <li>Name: {dentist.name}</li>
-              <li>Username:{dentist.username} </li>
-            </ul>
-            <div className="card__side card__side--back card__side--back-1">
-                <div className="card__cta">
-                  <div className="card__price-box">
-                    <p className="card__price-only">{dentist.name}</p>
-                  </div>
-                  <a href="#popup" className="btn btn--white">Book now!</a>
-                  <a href="#popup" className="btn btn--white">Show more details</a>
-                </div>
-              </div>
-          </div>
-          
+          <Card dentist={dentist} />
         ))}
-    </div>
-    </main >
-  )
-}
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
