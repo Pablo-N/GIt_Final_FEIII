45e0c0f295ee7c1c93f56938d03a028e44321f60
Detobone <detobone22@gmail.com>
2022-12-03 20:20:12 -0500

    Dark mode mocked

--- fe3-final-main/src/Components/Navbar.jsx
+++ fe3-final-main/src/Components/Navbar.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+// EL SWITCH Y EL USESTATE STAN MOCKEADOS
+//TODO: PASAR LOGICA DEL SWITCH THEM BY PROPS
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
 const Navbar = () => {
+  const [switchTheme, setsWitchTheme] = useState(false);
+
+  const handlerSwitchTheme = () => {
+    setsWitchTheme(!switchTheme);
+  };
+
   return (
     <header className="navigation">
       <nav className="navigation__container">
         <Link to="/home">Home</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/Favs">Favs</Link>
-        <button>Change theme</button>
+
+        <label class="switch">
+          <input onClick={handlerSwitchTheme} type="checkbox" />
+          <span class="switch__slider round">
+            {switchTheme ? (
+              <FontAwesomeIcon className="switch__sun" icon={faSun} />
+            ) : (
+              <FontAwesomeIcon className="switch__sun" icon={faMoon} />
+            )}
+          </span>
+        </label>
       </nav>
     </header>
   );
